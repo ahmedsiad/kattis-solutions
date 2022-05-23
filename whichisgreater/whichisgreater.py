d23065bfd52808ceaf9b889943fede54e02910ac
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-05-22 18:36:53 -0600

    "Which is Greater?", Difficulty: 1.3, comitted via Kattis Tracker

--- whichisgreater/whichisgreater.py
+++ whichisgreater/whichisgreater.py
@@ -2,10 +2,8 @@
 # Difficulty: 1.3
 # Time Limit: 1 second, Memory Limit: 2048 MB
 # CPU Time: 0.05 s
-# Author: 2021 ICPC North America Regionals (March 5 2022)
-# Source: 
-                                                                
-                                                                
+# Author: No author
+# Source: 2021 ICPC North America Regionals (March 5 2022)
 # Link: https://open.kattis.com/problems/whichisgreater
 
 
