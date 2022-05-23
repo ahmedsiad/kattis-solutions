42218a66ea8133bc693a74a823309a4cf5ebea0d
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-05-23 16:31:04 -0600

    "Two-sum", Difficulty: 1.3, comitted via Kattis Tracker

---
+++ twosum/twosum.py
@@ -0,0 +1,11 @@
+# ***Two-sum Solution***
+# Difficulty: 1.3
+# Time Limit: 1 second, Memory Limit: 1024 MB
+# CPU Time: 0.05 s
+# Author: Johan Sannemo
+# Source: Principles of Algorithmic Problem Solving
+# Link: https://open.kattis.com/problems/twosum
+
+
+a, b = list(map(int, input().split()))
+print(str(a + b))
