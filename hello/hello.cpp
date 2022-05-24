8f52b2ecd75627bc4ccdcd69d7a21c0f22960428
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-05-23 22:19:05 -0600

    "Hello World!", Difficulty: 1.1, comitted via Kattis Tracker

---
+++ hello/hello.cpp
@@ -0,0 +1,18 @@
+// ***Hello World! Solution***
+// Difficulty: 1.1
+// Time Limit: 5 seconds, Memory Limit: 1024 MB
+// CPU Time: 0.00 s
+// Author: No author
+// Source: Kattis
+// Link: https://open.kattis.com/problems/hello
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
