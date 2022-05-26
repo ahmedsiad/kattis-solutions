b240c4f3c59edf47e7d04cdacdbbe2a6fbd53e99
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-05-26 15:25:49 -0600

    "Solving for Carrots", Difficulty: 1.3, comitted via Kattis Tracker

---
+++ carrots/carrots.py
@@ -0,0 +1,13 @@
+# ***Solving for Carrots Solution***
+# Difficulty: 1.3
+# Time Limit: 1 second, Memory Limit: 1024 MB
+# CPU Time: 0.05 s
+# Author: Johan Sannemo
+# Source: KTH Training
+# Link: https://open.kattis.com/problems/carrots
+
+
+a, b = list(map(int, input().split()))
+for _ in range(a):
+  k = input()
+print(b)
