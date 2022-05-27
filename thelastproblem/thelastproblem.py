41cad0150024890cfc53415db2152d4df5776264
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-05-27 17:48:16 -0600

    "The Last Problem", Difficulty: 1.6, comitted via Kattis Tracker

---
+++ thelastproblem/thelastproblem.py
@@ -0,0 +1,11 @@
+# ***The Last Problem Solution***
+# Difficulty: 1.6
+# Time Limit: 1 second, Memory Limit: 1024 MB
+# CPU Time: 0.05 s
+# Author: Robin Christopher Yu
+# Source: NUS Competitive Programming
+# Link: https://open.kattis.com/problems/thelastproblem
+
+
+s = input()
+print("Thank you, {0}, and farewell!".format(s))
