379a15fed368a24a447038ecda84dfb0af299c11
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-05-27 18:12:26 -0600

    "Roaming Romans", Difficulty: 1.5, comitted via Kattis Tracker

---
+++ romans/romans.py
@@ -0,0 +1,12 @@
+# ***Roaming Romans Solution***
+# Difficulty: 1.5
+# Time Limit: 1 second, Memory Limit: 1024 MB
+# CPU Time: 0.05 s
+# Author: Steven Zeil
+# Source: 2017 ICPC Mid-Atlantic Regional Practice Contest
+# Link: https://open.kattis.com/problems/romans
+
+
+n = float(input())
+x = round(n * 1000 * 5280/4854)
+print(x)
