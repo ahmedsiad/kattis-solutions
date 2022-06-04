48ef903d5064ef33c4d51c5807eacb2ae9560b05
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-06-03 18:54:09 -0600

    "Echo Echo Echo", Difficulty: 1.2, comitted via Kattis Tracker

---
+++ echoechoecho/echoechoecho.py
@@ -0,0 +1,11 @@
+# ***Echo Echo Echo Solution***
+# Difficulty: 1.2
+# Time Limit: 1 second, Memory Limit: 1024 MB
+# CPU Time: 0.05 s
+# Author: Greg Hamerly
+# Source: Kattis
+# Link: https://open.kattis.com/problems/echoechoecho
+
+
+s = input()
+print(s,s,s)
