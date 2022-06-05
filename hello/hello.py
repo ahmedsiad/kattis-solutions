8692eb356bf7365e41883690fa72e86822d52c20
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-06-04 21:18:36 -0600

    "Hello World!", Difficulty: 1.1, comitted via Kattis Tracker

---
+++ hello/hello.py
@@ -0,0 +1,10 @@
+# ***Hello World! Solution***
+# Difficulty: 1.1
+# Time Limit: 5 seconds, Memory Limit: 1024 MB
+# CPU Time: 0.04 s
+# Author: No author
+# Source: Kattis
+# Link: https://open.kattis.com/problems/hello
+
+
+print("Hello World!")
