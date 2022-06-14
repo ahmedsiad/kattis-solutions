50772bdec69df9711f6cc5dba21ab10b5a9b7a24
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-06-13 19:53:19 -0600

    "Add Two Numbers", Difficulty: Show, comitted via Kattis Tracker

---
+++ addtwonumbers/addtwonumbers.py
@@ -0,0 +1,11 @@
+# ***Add Two Numbers Solution***
+# Difficulty: Show
+# Time Limit: 1 second, Memory Limit: 1024 MB
+# CPU Time: 0.04 s
+# Author: Greg Hamerly
+# Source: Kattis
+# Link: https://open.kattis.com/problems/addtwonumbers
+
+
+a, b = list(map(int, input().split()))
+print(a+b)
