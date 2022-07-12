fc302a963fc6b4e785b8ee8a269de47baeb34ada
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-07-11 22:08:30 -0600

    "Which is Greater?", Difficulty: 1.2, comitted via Kattis Tracker

--- whichisgreater/whichisgreater.py
+++ whichisgreater/whichisgreater.py
@@ -1,12 +1,13 @@
 # ***Which is Greater? Solution***
-# Difficulty: 1.3
+# Difficulty: 1.2
 # Time Limit: 1 second, Memory Limit: 2048 MB
-# CPU Time: 0.05 s
+# Acceptance: 50%
+# CPU Time: 0.04Â s
 # Author: No author
 # Source: 2021 ICPC North America Regionals (March 5 2022)
 # Link: https://open.kattis.com/problems/whichisgreater
 
 
-a,  b = list(map(int, input().split()))
-
-print("1") if a > b else print("0")
+a, b = list(map(int, input().split()))
+print("0") if a <= b else print("1")
+#toez
