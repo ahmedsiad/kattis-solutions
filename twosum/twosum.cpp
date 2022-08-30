132bd14ca44815ad150fb3b34ab90ff281b67397
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-08-30 14:44:33 -0600

    "Two-sum", Difficulty: 1.4, comitted via Kattis Tracker

--- twosum/twosum.cpp
+++ twosum/twosum.cpp
@@ -1,23 +1,23 @@
 // ***Two-sum Solution***
-// Difficulty: 1.3
+// Difficulty: 1.4
 // Time Limit: 1 second, Memory Limit: 1024 MB
-// CPU Time: 0.00 s
+// Acceptance: 57%
+// CPU Time: 0.00Â s
 // Author: Johan Sannemo
 // Source: Principles of Algorithmic Problem Solving
 // Link: https://open.kattis.com/problems/twosum
 
 
 #include <iostream>
-
+#define ll long long
 using namespace std;
 
 int main()
 {
-    long long a, b;
+    ll a, b;
     while (cin >> a >> b) {
         cout << a + b << endl;
     }
 
     return 0;
 }
-
