e0b5e86f748fbcda5853f66db4949a19c80b0054
Ahmed Siadomar <63379888+ahmedsiad@users.noreply.github.com>
2022-09-11 19:32:12 -0600

    "Stuck In A Time Loop", Difficulty: 1.4, comitted via Kattis Tracker

---
+++ timeloop/timeloop.py
@@ -0,0 +1,13 @@
+# ***Stuck In A Time Loop Solution***
+# Difficulty: 1.4
+# Time Limit: 1 second, Memory Limit: 1024 MB
+# Acceptance: 50%
+# CPU Time: 0.04 s
+# Author: Jack Rosenthal
+# Source: Camp Inc
+# Link: https://open.kattis.com/problems/timeloop
+
+
+n = int(input())
+for i in range(n):
+    print("{0} Abracadabra".format(i + 1))
